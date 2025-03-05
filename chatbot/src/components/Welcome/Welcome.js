6144eabfbfee3f279c4358fea2d830a218664a5f
navi-04 <naveenrajthiyagarajan6@gmail.com>
2025-03-05 21:40:35 +0530

    completed the preparation
    added and deleted the necessary files for the project

---
+++ chatbot/src/components/Welcome/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Welcome.css';
+
+const Welcome = () => {
+  return (
+    <div className="welcome-container">
+      <h1>Welcome to My Website!</h1>
+      <p>We're glad you're here. Explore and enjoy the content.</p>
+    </div>
+  );
+};
+
+export default Welcome;
